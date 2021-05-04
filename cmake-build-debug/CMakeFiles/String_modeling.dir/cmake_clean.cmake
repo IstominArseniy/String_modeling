41f8db94075e0db0ab1c8794fb20f56b561eb9d5
IstominArseniy <istomin15arseniy@gmail.com>
2021-05-04 20:38:51 +0300

    initial commit2

---
+++ cmake-build-debug/CMakeFiles/String_modeling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/String_modeling.dir/main.cpp.o"
+  "String_modeling"
+  "String_modeling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/String_modeling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
